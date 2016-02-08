06df97f58b19aedc365b100593b26f32e5509ab4
enterth3r4in <AS600@VAS600-028.lv.schoolcraft.cc.mi.us>
2016-02-08 15:32:48 -0500

    Done

--- InClassDemoFour/InClassDemoFour.cs
+++ InClassDemoFour/InClassDemoFour.cs
@@ -19,8 +19,14 @@ namespace InClassDemoFour
     {
         static void Main(string[] args)
         {
-            int firstNumber = 0, secondNumber = 0;
+            Console.Write("Enter a name: ");
 
+            /***
+            int firstNumber = 0, secondNumber = 0, examScore = 0;
+            double firstDouble = 0.0, secondDouble = 0.0;
+            string myName, yourName;
+            ***/
+            /***
             Console.Write("Enter an integer: ");
             firstNumber = int.Parse(Console.ReadLine());
 
@@ -28,9 +34,83 @@ namespace InClassDemoFour
             secondNumber = int.Parse(Console.ReadLine());
 
             if (firstNumber == secondNumber)
-                Console.WriteLine("The numbers are equal.");
+            {
+                Console.WriteLine(firstNumber.ToString() + " = " + secondNumber.ToString());
+            }
+            else if (firstNumber > secondNumber)
+            {
+                Console.WriteLine(firstNumber.ToString() + " > " + secondNumber.ToString());
+            }
+            else
+            {
+                Console.WriteLine(firstNumber.ToString() + " < " + secondNumber.ToString());
+            }
+
+            Console.Write("Enter a double: ");
+            firstDouble = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter another double: ");
+            secondDouble = double.Parse(Console.ReadLine());
+
+            if (firstDouble == secondDouble)
+            {
+                Console.WriteLine(firstDouble.ToString() + " = " + secondDouble.ToString());
+            }
+            else if (firstDouble > secondDouble)
+            {
+                Console.WriteLine(firstDouble.ToString() + " > " + secondDouble.ToString());
+            }
+            else
+            {
+                Console.WriteLine(firstDouble.ToString() + " < " + secondDouble.ToString());
+            }
+            
+
+            Console.Write("Enter an exam score [0-100]: ");
+            examScore = int.Parse(Console.ReadLine());
+
+            if(examScore >= 90)
+            {
+                Console.WriteLine("A");
+            }
+            else if(examScore >= 80)
+            {
+                Console.WriteLine("B");
+            }
+            else if(examScore >= 70)
+            {
+                Console.WriteLine("C");
+            }
+            else if(examScore >= 60)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+            //Name + score, name + score, First name received grade, second name received grade
+
+            Console.Write("Enter my name: ");
+            myName = Console.ReadLine();
+
+            Console.Write("Enter your name: ");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine(myName.CompareTo(yourName));
+
+            if(myName == yourName)
+            {
+                Console.WriteLine("Wow, that is weird.");
+            }
+            else
+            {
+                Console.WriteLine("We are humans.");
+            }
+
 
             Console.WriteLine("Program Done.");
+            ***/
         }
     }
 }
