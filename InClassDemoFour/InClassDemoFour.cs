d9e46b45d61a7a26ff9c95f1984ede368393ba35
enterth3r4in <AS600@VAS600-028.lv.schoolcraft.cc.mi.us>
2016-02-08 14:49:01 -0500

    In Class work

---
+++ InClassDemoFour/InClassDemoFour.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+ * Author: Cody Miller
+ * Assignment: In Class Demo Four
+ * Description: Program to demonstrate if statements
+ * Inputs: Accepts user input
+ * Outputs: Displays output conditionally
+ * Testing: run program expected out is varied.
+ **/
+
+namespace InClassDemoFour
+{
+    class InClassDemoFour
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0, secondNumber = 0;
+
+            Console.Write("Enter an integer: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another integer: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            if (firstNumber == secondNumber)
+                Console.WriteLine("The numbers are equal.");
+
+            Console.WriteLine("Program Done.");
+        }
+    }
+}
